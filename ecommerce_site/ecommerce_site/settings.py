791c3b58770b8d9d0d17016ab9b104ee4668ec2b
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-10 18:23:18 +0000

    errors, troubleshooting commit

--- ecommerce_site/ecommerce_site/settings.py
+++ ecommerce_site/ecommerce_site/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'pillow',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
