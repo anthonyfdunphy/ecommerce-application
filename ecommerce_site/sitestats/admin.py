7b696250621c3bc5a768b99f8cc18da3f48de006
anthonyfdunphy <anthonyfdunphy@gmail.com>
2023-02-18 10:37:26 +0000

    made admin panel that shows the webpage counts for a webpage

--- ecommerce_site/sitestats/admin.py
+++ ecommerce_site/sitestats/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import SiteStats
 
 class SiteStatsAdmin(admin.ModelAdmin):
-    list_display = ('url', 'views')
+    list_display = ('url', 'views', 'webpage')
 
 admin.site.register(SiteStats, SiteStatsAdmin)
